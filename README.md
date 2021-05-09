97b5d565d4d4f83adc86317eaa7a444a580e3241
Shubh_ank <62099471+5h0bh4nk@users.noreply.github.com>
2021-05-09 16:17:39 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# ThriveHer (Technica 2020)
+A platform for womxn entrepreneurs and business owners to pitch their ideas and get guidance from a mentor, and also get funding to get started.
+
+## Contributors
+Alice Huang, Jacquelyn Chow, Sojin Lim
+
+## Logo
+![thriveher](https://user-images.githubusercontent.com/51340361/97111397-d47fc600-16b4-11eb-8085-87438b5119f6.jpg)
+
+## Demo Video
+[Click here for demo video](https://youtu.be/Y2aJDZ4DTew)
+
+## Inspiration
+We decided to build ThriveHer because supporting small businesses and womenx entrepreneurs/business owners is always a pressing issue, but even more so now during the pandemic. We hope this will be a valuable tool for women and non-binary entrepreneurs to form local connections with one another, gain traction in the small business world, and find the key insights they need to make the next leap in their businesses’ journeys. We hope this will empower more women to build successful, influential businesses that will really benefit the whole community!
+
+## What it does
+Through ThriveHer, users can gain funding for their entrepreneurial ideas and learn from experienced mentors through 1-1 conversations.
+
+## Tools we used
+- Flask app (Python)
+- mongoDB for storing user info and mentor-mentee conversations
+- HTML/CSS, Bootstrap for frontend
+- JavaScript, frontend
+- AJAX, JQuery, SocketIO, for instant chat feature
+
+## Challenges we ran into
+- Collaborating virtually/in different time zones
+- Integrating all the features/db in limited timespan
+- Learning/incorporating new technologies
+
+## Accomplishments that we're proud of
+- Mentorship w/ established business owners - 1:1 conversations
+- Crowdfunding for women entrepreneurs to help build up capital
+
+## What we learned
+- Learned how to use and implement a non relational database with mongoDB
+- How to build a real-time chat feature using socket.io
+- Learned how to use Bootstrap 4
+
+## What's next for ThriveHer
+- Discussions page on general business qs, resources listing/shared links
+- Allow mentees to choose more than 1 mentor if the community grows
+- More incentives for people to mentor
+
+## Website Screenshots 
+###### Home Page
+<img src="./images/home.png" alt="Home Page" width="700"/>
+
+###### Tailor your account to your goals for growth
+<img src="./images/login.png" alt="Home Page" width="700"/>
+
+###### Get inspiration from experienced businesses
+<img src="./images/inspo.png" alt="Home Page" width="700"/>
+
+###### Chat 1-1 with your mentor
+<img src="./images/convo.png" alt="Home Page" width="700"/>
+
+###### Crowdfunding so womxn can support one another
+<img src="./images/crowdfunding.png" alt="Home Page" width="700"/>
+
+###### Feedback to improve your entrepreneurial pitch
+<img src="./images/forum.png" alt="Home Page" width="700"/>
+
---
+++ init.py
@@ -0,0 +1,103 @@
+from flask import Flask, render_template, url_for, request, session, redirect
+from pymongo import MongoClient
+
+import bcrypt
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.secret_key = 'mysecret'
+socketio = SocketIO(app)
+
+client = MongoClient('mongodb+srv://Sojin:technica@cluster0.e0mte.mongodb.net/<Technica>?ssl=true&ssl_cert_reqs=CERT_NONE')
+db = client.Users
+
+
+# index route
+@app.route("/")
+def index():
+    if 'username' in session:
+        return render_template("index.html", user=session['username'])
+    return render_template("index.html")
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    
+    if request.method == 'POST':
+        users = db.Users
+        login_user = users.find_one({'name' : request.form['username']})
+
+        if login_user:
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['pw']) == login_user['pw']:
+                session['username'] = request.form['username']
+                return redirect(url_for('index'))
+
+        return 'Invalid username/password combination'
+    return render_template("login.html")
+
+@app.route("/convos", methods=["GET"])
+def convos():
+    if session['username']:
+        user = session['username']
+        return render_template("convos.html", user=user)
+    return redirect(url_for('login'))
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == 'POST':
+
+        if not request.form.get("username"):
+            return "Please provide a username!"
+        elif not request.form.get("password"):
+            return "Please provide a password!"
+        elif not request.form.get("confirmation"):
+            return "Please confirm your password!"
+        
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        users = db.Users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+
+            if password != request.form.get("confirmation"):
+                return 'Password doesn\'t match confirmation!'
+
+            hashpass = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'pw' : hashpass, 'preferences' : request.form.getlist('options')})
+            session['username'] = username
+            return redirect(url_for('index'))
+    
+        return 'That username already exists!'
+
+    else:
+        return render_template("signup.html")
+
+@app.route("/inspiration", methods=["GET"])
+def inspiration():
+    return render_template("inspo.html", user=session["username"])
+
+@app.route("/funding", methods=["GET"])
+def funding():
+    return render_template("funding.html", user=session["username"])
+
+@app.route("/feedback") 
+def feedback():
+    return render_template("feedback.html")
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    session['username'] = ""
+    return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    app.run()
